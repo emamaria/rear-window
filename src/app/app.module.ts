9b082a992376f4f1d5e374f8edbeb509f53b1a09
emamaria <emascript@gmail.com>
2023-03-19 11:26:38 +0100

    ok footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { CastsComponent } from './pages/casts/casts.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { TechnicalComponent } from './pages/technical/technical.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CastsComponent,
+    CharactersComponent,
+    TechnicalComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/casts/casts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-casts',
+  templateUrl: './casts.component.html',
+  styleUrls: ['./casts.component.css']
+})
+export class CastsComponent {
+
+}
