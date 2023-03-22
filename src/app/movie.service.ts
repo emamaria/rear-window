ac7476916ee26930371257c3018000a11a9358c0
emamaria <emascript@gmail.com>
2023-03-22 19:24:36 +0100

    main characters

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -9,6 +9,15 @@ export class MoviesService {
 
   private apiKey:string = environment.apiKey
 
+  mainCharacters: any[] = [
+    {name: "L. B. Jefferies", img: "character1.jpg", role: "Jefferies, a celebrated photojournalist."},
+    {name: "Lisa Carol Fremont", img: "character2.jpg", role: "Lisa Fremont, Jeff´s glamourous girlfriend."},
+    {name: "Stella", img: "character3.jfif", role: "Jeff´s nurse."},
+    {name: "Det.Lt.Thomas", img: "character4.jpg", role: "Detective and Jeff´s old friend."},
+    {name: "Lars Thorwald", img: "character5.jfif", role: "The suspicious neighbor."},
+    {name: "Miss Lonelyhearts", img: "character6.jfif", role: "Neighbor and  a single woman who is obssesed over finding love."}
+  ]
+
  options = {
     headers: {
       'X-RapidAPI-Key': this.apiKey,
@@ -18,7 +27,7 @@ export class MoviesService {
   }
   
   constructor(private http: HttpClient){ 
-     
+    
    
    
   }
@@ -28,6 +37,10 @@ export class MoviesService {
     
     let url = 'https://online-movie-database.p.rapidapi.com/title/get-plots'
     return this.http.get(url, this.options);
+   }
+
+   getCharacters(){
+    return this.mainCharacters
    }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,12 +11,17 @@ import { MoviesService } from '../../movie.service';
 export class HomeComponent {
 
   public plot:string = "Professional photographer L.B. 'Jeff' Jefferies breaks his leg while getting an action shot at an auto race. Confined to his New York apartment, he spends his time looking out of the rear window observing the neighbors. He begins to suspect that a man across the courtyard may have murdered his wife. Jeff enlists the help of his high society fashion-consultant girlfriend Lisa Fremont and his visiting nurse Stella to investigate."
-  public characters: string[] = []
+  
+  
 
+  get getCharacters(){
+
+    return this.api.getCharacters()
+  }
   
 
   constructor(private api: MoviesService){
-    // this.getPlot()
+ 
    }
  
    getPlot(): any {
@@ -28,5 +33,7 @@ export class HomeComponent {
     })
       
     }
+
+   
 
 }
