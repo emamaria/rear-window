d711bf9d835372ba97b0c812597704302d4aa24a
emamaria <emascript@gmail.com>
2023-07-19 14:14:26 +0200

    change plot

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,7 +10,7 @@ import { MoviesService } from '../../movie.service';
 })
 export class HomeComponent {
 
-  public plot:string = `Professional photographer L. B. Jefferies has broken his leg and must rest at home.
+  public plot:string = `Professional photojournalist L. B. Jefferies has broken his leg and must rest at home.
   The boredom of being confined, leads him to observe what the neighbors are doing from his window.
   One day he observes a suspicious behavior from a neighbor who may have murdered his wife. Jeff will try to investigate
   what has happened with the help of his girlfriend Lisa and his nurse Stella.`
